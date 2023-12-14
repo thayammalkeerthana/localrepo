4b5325ce633554d1d3c9e776aa201e33cd516c97
keerthana <thayammalkeerthana@gmail.com>
2023-12-14 17:05:49 +0530

    add the author name

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="repo_txt">
+    <div>
+      <div className="repo_txt">
      this is my local repo
+    </div>
+    <div>author : keerthana</div>
     </div>
   );
 }
