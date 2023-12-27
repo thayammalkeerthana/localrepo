08f481d8abc4665566b5718fa82919d86fffa94a
keerthana <thayammalkeerthana@gmail.com>
2023-12-27 19:43:41 +0530

    update the app content second time

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import './App.css';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <hl className="repo_txt" style={{ marginTop: '5px' }}>Hello World!!!!!!!</hl>
       <div className="repo_txt" style={{ marginTop: '5px' }}>
         this is my local repo
       </div>
-=======
-      <div className="repo_txt">
-     this is my local repo
->>>>>>> 4b5325ce633554d1d3c9e776aa201e33cd516c97
     </div>
-    <div>author : keerthana</div>
-    </div>
+
   );
 }
 
